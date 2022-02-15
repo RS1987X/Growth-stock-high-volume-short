5312533cc5fa38f47826e0d65f625fd3e4c95d48
RS1987X <richard.kj.steffen@gmail.com>
2022-02-16 00:00:58 +0100

    data

--- average opening range volumes.py
+++ average opening range volumes.py
@@ -19,7 +19,7 @@ from statsmodels.graphics.tsaplots import plot_acf
 tickers = ["EVO", "SINCH", "LATO_B", "KINV_B", "NIBE_B", "EQT", "MIPS", "STORY_B", "SF", "PDX", "SBB_B", "BALD_B", "SAGA_B", "INDT", "LIFCO_B", "LAGR_B"]
 
 for x in tickers:
-    
+    #print(x)
     data = pd.read_csv('OMXSTO_DLY_'+ x + ', 15.csv')
     #data = data['time,open,high,low,close,VWAP,Upper Band,Lower Band,Volume,Volume MA'].str.split(",",expand=True)
     #data = data.rename(columns={0:"DateTime", 1:"Open", 2:"High", 3:"Low", 4:"Close", 5:"VWAP", 6:"Upper Band", 7:"Lower Band", 8:"Volume", 9:"Volume MA"})
@@ -38,7 +38,7 @@ for x in tickers:
     opening_rng_volume = opening_rng_volume.set_index("DatePart")
     
     #calculate rolling 20 session opening range volume
-    avg_rolling_opening_volume = opening_rng_volume.rolling(20).mean().shift(1)
+    avg_rolling_opening_volume = opening_rng_volume.rolling(20).mean()#.shift(1)
     #print(x)
     print(avg_rolling_opening_volume.tail(1)["Volume"][0])
     
